deea2eecaba4e09380e1c1cfbc334ff4eed2111d
JuLuogo <1576586736@qq.com>
2025-10-17 20:48:39 +0800

    ci(workflow): 使用 npm ci 替代 npm install 以提高依赖安装可靠性
    refactor(worker): 移除不再需要的静态资源绑定配置
    删除 public 目录及相关 HTML 文件，简化项目结构
    
    chore(wrangler): 更新中间件打包文件

--- .wrangler/tmp/dev-sxsXBx/worker.js
+++ .wrangler/tmp/dev-sxsXBx/worker.js
@@ -3022,7 +3022,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-B5UKiG/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-oyMnYS/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -3054,7 +3054,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-B5UKiG/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-oyMnYS/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
